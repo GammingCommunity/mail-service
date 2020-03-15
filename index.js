bfdfa1db1983ae6818cfb9adc299c9033e570573
Mattstacey <mattstacey@pop-os.localdomain>
2020-03-15 23:52:35 +0700

    add detected device

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 
 const express = require('express');
-const app= express();
-const {verify} = require('jsonwebtoken');
-const {generateCode} = require('./src/generateCode');
-const {sendMail} = require('./src/sendEmail');
+const app = express();
+const { verify } = require('jsonwebtoken');
+const { generateCode } = require('./src/generateCode');
+const { sendMail } = require('./src/sendEmail');
+var device = require('express-device');
 require('dotenv').config()
-
-app.post("/validation",async (req,res)=>{
+app.use(device.capture());
+app.post("/validation", async (req, res) => {
     const userToken = req.get('token');
-    const userID =req.get('userID')
-    const userEmail= req.get('userEmail');
-   try {
-       // generate code here
-       var code = generateCode();
-       var token = verify(userToken, process.env.mail_jwt_secret_key);
-       if(token.id == userID){
-           var result = await sendMail(userEmail, userID, code);
-           if (result == true) {
-               res.send("Email sent ! Check your inbox");
-           }
-           else res.send("Try again")
-       }
-       else res.send("Wrong credenital...")
+    const userID = req.get('userID')
+    const userEmail = req.get('userEmail');
+    try {
+        // generate code here
+        var code = generateCode();
+        var token = verify(userToken, process.env.mail_jwt_secret_key);
+        if (token.id == userID) {
+            var result = await sendMail(userEmail, userID, code);
+            if (result == true) {
+                res.send("Email sent ! Check your inbox");
+            }
+            else res.send("Try again")
+        }
+        else res.send("Wrong credenital...")
 
-   } catch (error) {
-    console.log(error);
-       res.send(error);
-   }
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 });
-app.get('/verify',async(req,res)=>{
+app.get('/verify', async (req, res) => {
     const confirmCode = req.get('confirmCode');
     console.log(confirmCode);
-    if(confirmCode != null){
+    console.log(req.device);
+
+    if (confirmCode != null) {
         res.send("Verified.")
-    }else res.send('Try again')
-    
+    } else res.send('Try again')
+
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Listen on ${PORT}`);
 })
